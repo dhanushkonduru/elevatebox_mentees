fd1de92dd9e9bcc1fa8cb35a93225a25ebea046a
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-07-29 09:36:16 +0530

    Print pyramid pattern.cpp

---
+++ Hackerrank/Print pyramid pattern.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N - i - 1; j++) {
+            cout << " ";
+        }
+ 
+        for(int k = 0; k < (2 * i + 1); k++) {
+            cout << "*";
+        }
+ 
+        cout << endl;
+    }
+    
+    return 0;
+}
