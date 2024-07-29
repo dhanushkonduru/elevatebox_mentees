ef9a32ee47d613ee775c683613b9f9978add2051
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-07-29 09:41:16 +0530

    Print multiplication table.cpp

---
+++ Hackerrank/Print multiplication table.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << N << " * " << i << " = " << N * i << endl;
+    }
+    return 0;
+}
