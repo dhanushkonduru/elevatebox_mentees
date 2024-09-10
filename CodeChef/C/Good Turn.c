ce0aeda3a0b975ed65882af792546fcdfe1e219d
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-10 20:47:51 +0530

    Good Turn

---
+++ CodeChef/C/Good Turn.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    // Loop for each test case
+    while (t--) {
+        int x, y;
+
+      
+        scanf("%d %d", &x, &y);
+
+        // Your code for each test case goes here 
+        if((x+y) > 6) printf("Yes\n");
+        else printf("No\n");
+    }
+}
