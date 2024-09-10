7eba0caddb2fa1f754479c37a5c510e94571d358
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-10 09:10:36 +0530

    BMI Calculator

---
+++ CodeChef/C/BMI Calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float height = 1.82;
+    float weight = 72;
+    // Complete the code
+    float BMI = weight/(height*height);
+    printf("%f",BMI);
+    return 0;
+}
