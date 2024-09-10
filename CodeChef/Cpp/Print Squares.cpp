049bd546fda3a95f08fdb75f760677807681afe9
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-10 23:47:46 +0530

    Print Squares

---
+++ CodeChef/Cpp/Print Squares.cpp
@@ -0,0 +1,10 @@
+ #include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n = 5;
+    for(int i = 1; i <= n; i++){
+        cout << i << "-" << i*i << endl;
+    }
+}
