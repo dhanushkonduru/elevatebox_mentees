b8f07a25f9631c8c918484746e44eaf53773f222
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-10 09:55:26 +0530

    Even Or Odd

---
+++ CodeChef/C/Even Or Odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int n;
+    scanf("%d",&n);
+    if(n/2 != 0){
+        printf("Odd");
+    }
+    else{
+        printf("Yes");
+    }
+}
+
