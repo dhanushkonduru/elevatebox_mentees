65106a4c47e1191d8c5173bb6996284abca7c4f8
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-10 23:55:02 +0530

    Print all the arithematic operations

---
+++ CodeChef/Cpp/Print all the arithematic operations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your code here
+    cout << 8+4 << endl;
+    cout << 8-4 << endl;
+    cout << 8/4 << endl;
+    cout << 8*4 << endl;
+}
