5422ca4b980cbac00c4e05c44f74f477a4869a40
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-11 00:19:13 +0530

    Concert Temperature

---
+++ CodeChef/Cpp/Convert Temperature.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    float temperature = 25.5;
+    cout << "Celsius - " << temperature << endl;
+    cout << "Kelvin - " << temperature+273;
+}
