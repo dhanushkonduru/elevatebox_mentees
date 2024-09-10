35e9b899bfe0d003a2aa80110e063c6c1dc7fcbb
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-09-10 10:16:10 +0530

    Even Sum

---
+++ CodeChef/C/Even Sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    // Take inputs and complete the code
+    scanf("%d %d",&a,&b);
+    if((a+b)%2 == 0) printf("YES");
+    else printf("NO");
+    return 0;
+}
