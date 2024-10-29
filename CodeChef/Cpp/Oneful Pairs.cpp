cc4342047b1dae93b846db4ab8c5ab06c047e1c9
dhanushkonduru <164062577+dhanushkonduru@users.noreply.github.com>
2024-10-29 12:09:34 +0530

    Oneful Pairs

---
+++ CodeChef/Cpp/Oneful Pairs.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+	// your code goes here
+	if(a+b+(a*b) == 111) cout << "Yes";
+	else cout << "No";
+	return 0;
+}
